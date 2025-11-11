e7cfd21a6d0d01690b16180f084d34a37cadb72a
Alberth Ruiz <ch.alb.rz@hotmail.com>
2025-11-10 21:13:38 -0500

    Update

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -2,8 +2,3 @@
 # Instala el servidor web Apache
 yum install -y httpd
 
-#!/bin/bash
-# Inicia el servidor web y lo habilita para que inicie con el
-sistema
-service httpd start
-chkconfig httpd on
